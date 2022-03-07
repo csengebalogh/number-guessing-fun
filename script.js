92c640a231b7f55446da1ea9d7e05fff264c04aa
csengekulker <k71608883429@ktch.hu>
2022-03-07 17:16:32 +0100

    error, wip (async?)

--- script.js
+++ script.js
@@ -1,63 +1,38 @@
 const readline = require("readline")
-
 const cl = console.log
-
-function genNumber () {
-    return (Math.round(Math.random() * 10))
-}
-
-
-var generated = genNumber()
-
-
-cl("I generated a random integer from 0 to 10, you make a guess")
-cl("I will give you hints after each guess")
-
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
+cl("I generated a random integer from 0 to 10, you make a guess")
+cl("I will give you hints after each guess")
 
+var generated = (Math.round(Math.random() * 10))
 
 rl.setPrompt("Think about an integer from 0 to 10 > ")
 rl.prompt()
-// rl.on('line', (age) => {
-//     console.log(`Age received by the user: ${age}`);
-//     rl.close();
-// });
+
+var tries = 3
 
 
+rl.on('line', ans => {
+    let answer = global.ans
+})
 
+ // ASYNC AWAIT 
 
-for (var i = 0; i<3; i++) {
-    rl.on('line', (ans) => {
-        // var generated = genNumber()
-    
-        cl("Your guess:", ans)
-        cl("Generated:", generated)
-    
-        if (generated == ans) {
+while (tries != 0) {
+
+    switch(answer) {
+        case answer == generated:
             cl("match")
-            rl.close()
-        } else {
-            cl("no match")
-            newGuess()
-        }
-    
-    })
-}
-
-//kell egy ciklus  csak nemtudom hova 
-
-function newGuess () {
-    rl.setPrompt("Make a new guess > ")
-    rl.prompt()
-    rl.on('line', (ans) => {
-        if (ans == generated) {
-            cl("match")
-        } else {
-            cl("no match second time")
-        }
-    })
+            break;
+        case answer < generated:
+            cl("ennel kisebb")
+            break
+        case answer > generated:
+            cl("ennel nagyobb  ")
+            break
+    }
 }
